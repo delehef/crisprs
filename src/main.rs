bf255d6ff4ff5764fc440ab5efcc397e4dfeb7f4
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-02-01 13:40:43 +0100

    First functional implementation of backtrans

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
-use anyhow::Result;
-use clap::Parser;
+use clap::app_from_crate;
+use clap::*;
+use color_eyre::eyre::{Result, WrapErr};
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::BufWriter;
+use std::io::Write;
 mod fasta;
 use fasta::*;
 use std::collections::HashMap;
 
-fn backtrans(
+fn backtrans<'a>(
     proteins_filename: &str,
     dna_filename: &str,
-    mut out: impl std::io::Write,
+    mut out: BufWriter<Box<dyn std::io::Write + 'a>>,
 ) -> Result<()> {
-    let dna = FastaReader::new(File::open(dna_filename)?, true)
-        .map(|r| (r.id, r.seq.unwrap()))
-        .collect::<HashMap<_, _>>();
-    for protein in FastaReader::new(File::open(proteins_filename)?, true) {
+    let dna = FastaReader::new(
+        File::open(dna_filename).wrap_err(format!("while opening {}", dna_filename))?,
+        true,
+    )
+    .map(|r| (r.id, r.seq.unwrap()))
+    .collect::<HashMap<_, _>>();
+    for protein in FastaReader::new(
+        File::open(proteins_filename).wrap_err(format!("while opening {}", dna_filename))?,
+        true,
+    ) {
+        const PER_LINE: usize = 20;
         let dna_sequence = dna.get(&protein.id).unwrap();
         let prot_sequence = protein.seq.as_ref().unwrap();
-        assert!(dna_sequence.len() == 3 * prot_sequence.len());
+        assert!(dna_sequence.len() >= 3 * prot_sequence.iter().filter(|x| **x != b'-').count());
 
         writeln!(
             out,
@@ -46,9 +55,51 @@ fn backtrans(
         }
         writeln!(out)?;
     }
-    Ok(())
+    Ok(out.flush()?)
 }
 
-fn main() {
-    backtrans("pipo.pp", "pipo.fa", File::open("out.test").unwrap());
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let main_args = app_from_crate!()
+        .global_setting(AppSettings::PropagateVersion)
+        .global_setting(AppSettings::UseLongFormatForHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("backtrans")
+                .about("Convert a protein FASTA file to a DNA FASTA file")
+                .arg(
+                    Arg::new("PROTEIN_FILE")
+                        .long("from")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("DNA_FILE")
+                        .long("with")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(Arg::new("OUT_FILE").long("to").takes_value(true)),
+        )
+        .get_matches();
+
+    match main_args.subcommand() {
+        Some(("backtrans", args)) => {
+            let stdout = std::io::stdout();
+            let out: BufWriter<Box<dyn std::io::Write>> =
+                if let Some(out_filename) = args.value_of("OUT_FILE") {
+                    BufWriter::with_capacity(30_000_000, Box::new(File::create(out_filename)?))
+                } else {
+                    BufWriter::new(Box::new(stdout.lock()))
+                };
+
+            backtrans(
+                args.value_of("PROTEIN_FILE").unwrap(),
+                args.value_of("DNA_FILE").unwrap(),
+                out,
+            )
+        }
+        _ => unreachable!(),
+    }
 }
