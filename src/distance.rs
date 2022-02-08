5f264732d017754642e134d4fe818a42f885b18b
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-02-08 11:53:17 +0100

    Add levenshtein_avx2

--- src/distance.rs
+++ src/distance.rs
@@ -1,11 +1,7 @@
 use crate::fasta::*;
 use color_eyre::eyre::{Result, WrapErr};
-use log::*;
 use std::arch::x86_64::*;
-use std::fs::File;
 use std::io::{BufWriter, Read, Write};
-
-const SIMD_WIDTH: usize = 32;
 
 pub enum Distance {
     Levenshtein,
@@ -59,28 +55,30 @@ fn kimura_x86(s1: &[u8], s2: &[u8]) -> (usize, usize) {
         })
 }
 
+unsafe fn print_mm256(a: __m256i) {
+    let mut o = vec![b'_'; 32];
+    _mm256_store_si256(o.as_mut_ptr() as *mut __m256i, a);
+    println!("{}", std::str::from_utf8(&o).unwrap());
+}
+
+unsafe fn print_bool256(a: __m256i) {
+    let mut o = vec![b'_'; 32];
+    _mm256_store_si256(o.as_mut_ptr() as *mut __m256i, a);
+    o.iter_mut().for_each(|b| {
+        if *b == 0xff {
+            *b = b'.'
+        }
+        if *b == 0x0 {
+            *b = b' '
+        }
+    });
+    println!("{}", std::str::from_utf8(&o).unwrap());
+}
+
+
 #[target_feature(enable = "avx2")]
 unsafe fn kimura_avx2(s1: &[u8], s2: &[u8]) -> (usize, usize) {
-    unsafe fn print_mm256(a: __m256i) {
-        let mut o = vec![b'_'; 32];
-        _mm256_store_si256(o.as_mut_ptr() as *mut __m256i, a);
-        println!("{}", std::str::from_utf8(&o).unwrap());
-    }
-
-    unsafe fn print_bool256(a: __m256i) {
-        let mut o = vec![b'_'; 32];
-        _mm256_store_si256(o.as_mut_ptr() as *mut __m256i, a);
-        o.iter_mut().for_each(|b| {
-            if *b == 0xff {
-                *b = b'.'
-            }
-            if *b == 0x0 {
-                *b = b' '
-            }
-        });
-        println!("{}", std::str::from_utf8(&o).unwrap());
-    }
-
+    const SIMD_WIDTH: usize = 32;
     let n = s1.len();
     let lasts = n % SIMD_WIDTH;
     let (mut matches, mut scored) = s1[n - lasts..n]
@@ -107,19 +105,61 @@ unsafe fn kimura_avx2(s1: &[u8], s2: &[u8]) -> (usize, usize) {
         scored += SIMD_WIDTH - _mm256_movemask_epi8(either_dashes).count_ones() as usize;
     }
 
-    // println!("AVX ===\n{} -- {} ==> {}", matches, scored, d);
     (matches.try_into().unwrap(), scored)
 }
 
 fn levenshtein(s1: &[u8], s2: &[u8]) -> f64 {
+        #[cfg(target_feature = "avx2")]
+        unsafe {
+            levenshtein_avx2(s1, s2)
+        }
+        #[cfg(not(target_feature = "avx2"))]
+        {
+            levenshtein_x86(s1, s2)
+        }
+}
+
+#[target_feature(enable = "avx2")]
+unsafe fn levenshtein_avx2(s1: &[u8], s2: &[u8]) -> f64 {
+    const SIMD_WIDTH: usize = 32;
+    let n = s1.len();
+    let lasts = n % SIMD_WIDTH;
+    let (mut matches, mut scored) = s1[n - lasts..n]
+        .iter()
+        .zip(s2[n - lasts..n].iter())
+        .filter(|(&n1, &n2)| !(n1 == b'-' && n2 == b'-'))
+        .fold((0, 0), |(matches, scored), (n1, n2)| {
+            (matches + if n1 == n2 { 1 } else { 0 }, scored + 1)
+        });
+
+    let dashesx32 = _mm256_set1_epi8(b'-'.try_into().unwrap());
+    for i in (0..n - lasts).step_by(SIMD_WIDTH) {
+        let a = _mm256_loadu_si256(s1.as_ptr().offset(i.try_into().unwrap()) as *const __m256i);
+        let b = _mm256_loadu_si256(s2.as_ptr().offset(i.try_into().unwrap()) as *const __m256i);
+
+        let a_dashes = _mm256_cmpeq_epi8(a, dashesx32);
+        let b_dashes = _mm256_cmpeq_epi8(b, dashesx32);
+
+        let both_dashes = _mm256_and_si256(a_dashes, b_dashes); // Levenshtein: gaps are *not* ignored
+
+        let a_eq_b = _mm256_andnot_si256(both_dashes, _mm256_cmpeq_epi8(a, b));
+
+        matches += _mm256_movemask_epi8(a_eq_b).count_ones();
+        scored += SIMD_WIDTH - _mm256_movemask_epi8(both_dashes).count_ones() as usize;
+    }
+
+    1.0 - matches as f64/scored as f64
+}
+
+fn levenshtein_x86(s1: &[u8], s2: &[u8]) -> f64 {
     let (matches, scored) = s1
         .iter()
         .zip(s2.iter())
         .filter(|(&n1, &n2)| !(n1 == b'-' && n2 == b'-'))
-        .fold((0f64, 0f64), |(matches, scored), (n1, n2)| {
-            (matches + if n1 == n2 { 1. } else { 0. }, scored + 1.)
+        .fold((0, 0), |(matches, scored), (n1, n2)| {
+            (matches + if n1 == n2 { 1 } else { 0 }, scored + 1)
         });
-    1.0 - matches / scored
+    1.0 - matches as f64 / scored as f64
 }
 
 pub fn distance<T: Read>(fasta: FastaReader<T>, distance: Distance) -> (Vec<String>, Vec<f64>) {
